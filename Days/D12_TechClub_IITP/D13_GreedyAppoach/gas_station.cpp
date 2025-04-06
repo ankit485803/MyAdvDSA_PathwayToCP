87ee4af22c719e418e911a8a7fffac39066d093a
Ankit Kumar <ankit485803@gmail.com>
2025-04-06 11:00:05 +0530

    solve gasStation prob using greedy approach and setup all folder for LinearDS and NonLinar DS

---
+++ Days/D12_TechClub_IITP/D13_GreedyAppoach/gas_station.cpp
@@ -0,0 +1,94 @@
+
+
+/*
+ qno 134  https://leetcode.com/problems/gas-station/description/
+
+ concept used: array and greedy
+
+*/
+
+
+class Solution {
+    public:
+        int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+            int totGas = 0, totCost = 0;
+    
+            for(int val : gas) {
+                totGas += val;
+            }
+    
+            for(int val : cost) {
+                totCost += val;
+            }
+    
+    
+            if(totGas < totCost) {
+                return -1;
+            }
+    
+    
+            //unique solution always exists
+            int start = 0, currGas = 0;
+    
+            for(int i=0; i<gas.size(); i++) {
+                currGas += (gas[i] - cost[i]);
+    
+                if(currGas < 0) {
+                    start = i+1;
+                    currGas = 0;
+                }
+            }
+    
+            return start;
+            
+        }
+    };
+    
+    
+    // using Greedy approach, TC = O(n), SC = O(1)
+
+
+
+
+//optimize 
+
+
+class Solution {
+    public:
+        int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+            int totGas = 0, totCost = 0;
+    
+    
+    
+    
+            //unique solution always exists
+            int start = 0, currGas = 0;
+    
+            for(int i=0; i<gas.size(); i++) {
+                //optimize calculate gas here instead of outside
+                totGas += gas[i];
+                totCost += cost[i];
+    
+                currGas += (gas[i] - cost[i]);
+    
+                if(currGas < 0) {
+                    start = i+1;
+                    currGas = 0;
+                }
+            }
+    
+            
+            // if(totGas < totCost) {
+            //     return -1;
+            // }
+    
+            // return start;
+    
+    
+            return totGas < totCost ? -1 : start;
+            
+        }
+    };
+    
+    
+    // using Greedy approach, TC = O(n), SC = O(1)
