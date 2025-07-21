e1241e346718f7ebc2822e1feba44316679563d9
Ankit Kumar <ankit485803@gmail.com>
2025-07-21 16:31:23 +0530

    Morris Inorder traversal algo and recover BST

---
+++ Chapter_DS/NonLinear/02_BST/Calculate_InorderTraver_UsingIterative.cpp
@@ -0,0 +1,16 @@
+
+
+
+/*
+
+20th July 2025 (Monday ) Calculate Inorder Traversal BST, or any tree; Using Iterative method not recursive which is easy i.e. MORRIS 
+inorder traversal method
+
+Inorder Predecessor: IP -- eska matlab right most node in left subtree 
+
+qno 94 leetCode https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
+
+Chapter_DS\NonLinear\01_BinaryTree\Height_of_BT\L94_Morris_InorderTraversal_BT.cpp
+
+*/
--- Chapter_DS/NonLinear/02_BST/Que/L104_Recover_BST.cpp
+++ Chapter_DS/NonLinear/02_BST/Que/L104_Recover_BST.cpp
@@ -82,3 +82,62 @@ C:\Users\sanja\Desktop\GitHub_ankit485803\MyAdvDSA_PathwayToCP\Chapter_DS\NonLin
 */
 
 
+class Solution {
+public:
+    void recoverTree(TreeNode* root) {
+        // Pointers to track the misplaced nodes and the previous visited node in inorder traversal
+        TreeNode* first = nullptr;
+        TreeNode* second = nullptr;
+        TreeNode* prev = nullptr;
+
+        // Start Morris Inorder Traversal
+        while (root != nullptr) {
+            // Case 1: If there is no left child, process current node and move to right
+            if (root->left == nullptr) {
+                
+                // Detect swapped nodes by checking if the previous node is greater than the current
+                if (prev && prev->val > root->val) {
+                    if (!first) {
+                        first = prev;      // First anomaly
+                    }
+                    second = root;         // Second anomaly
+                }
+
+                prev = root;               // Update previous node
+                root = root->right;        // Move to right subtree
+
+            } else {
+                // Case 2: If left child exists, find the rightmost node in left subtree (inorder predecessor)
+                TreeNode* pred = root->left;
+                while (pred->right && pred->right != root) {
+                    pred = pred->right;
+                }
+
+                // Threading: Create a temporary link from predecessor to current node
+                if (pred->right == nullptr) {
+                    pred->right = root;     // Establish the thread
+                    root = root->left;      // Move to left child
+                } else {
+                    // If thread already exists, it's time to remove it and process the current node
+                    pred->right = nullptr;  // Remove the thread
+
+                    // Again, check for anomalies
+                    if (prev && prev->val > root->val) {
+                        if (!first) {
+                            first = prev;
+                        }
+                        second = root;
+                    }
+
+                    prev = root;            // Update previous node
+                    root = root->right;     // Move to right subtree
+                }
+            }
+        }
+
+        // Swap the values of the two misplaced nodes to fix the tree
+        if (first && second) {
+            std::swap(first->val, second->val);
+        }
+    }
+};
