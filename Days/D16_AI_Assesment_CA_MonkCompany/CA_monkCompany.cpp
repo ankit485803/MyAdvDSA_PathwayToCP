2cc99e8c41c336e84df40892470799d19b4765a4
Ankit Kumar <ankit485803@gmail.com>
2025-08-08 16:28:11 +0530

    CA Monk company OA round

---
+++ Days/D16_AI_Assesment_CA_MonkCompany/CA_monkCompany.cpp
@@ -0,0 +1,35 @@
+
+
+
+
+/*
+
+8th Aug 2025 (Friday) Invitation to Complete Your AI Assessment – CA Monk Recruitment
+
+AboutCompany -- CA Monk is an upskilling platform for commerce and finance professionals, founded in 2020 by CA Shivam Palan.
+https://www.camonk.com/
+
+
+
+CA Monk <jobs@camonk.com>	7 August 2025 at 10:51
+Reply-To: support@camonk.com
+To: ankit485803@gmail.com
+Dear Ankit,
+Thank you for your interest in the Backend Developer position at CA Monk. As part of our recruitment process, we invite you to complete an AI-based assessment designed to help us better understand your skills and suitability for the role.
+
+Assessment Details:
+Assessment Link: https://screener.camonk.com/user/6d68d89d-7d79-4b1d-8b5b-c62857b21150
+Deadline for Completion: 08 August 2025 @ 12:01 PM
+
+Please ensure you complete the assessment by the specified deadline. The assessment is a crucial step in our selection process, and only candidates who complete it on time will be considered for the next stage.
+
+If you have any questions or face any technical difficulties, please feel free to reach out to us at support@camonk.com
+
+
+
+
+
+TQ=6q in 18 min but all are video + interview based give answer oral and share screen recording
+Ankit learns that we should sit the test to wear the formal shirt because camera is ON
+
+*/
