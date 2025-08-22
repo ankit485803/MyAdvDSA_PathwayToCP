720c542e955273e6c31a2713bc78825efec121ee
Ankit Kumar <ankit485803@gmail.com>
2025-08-22 14:12:31 +0530

    XOR after range multiple queries problem

--- 0_Arjuna_CP/D26 to D40/WeekyContest_463/Q2_XOR_AfterRangeMultipQueries.cpp
+++ 0_Arjuna_CP/D26 to D40/WeekyContest_463/Q2_XOR_AfterRangeMultipQueries.cpp
@@ -58,9 +58,45 @@ queries[i] = [li, ri, ki, vi]
 */
 
 
-class Solution {
+class Solution {  //resolved on 22nd Aug 2025 (Friday)
 public:
     int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
         
     }
 };
+
+
+
+
+class Solution {
+public:
+    int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        const int MOD = 1e9 + 7; // modulus to avoid overflow during multiplication
+
+        // Process each query one by one
+        for (auto &q : queries) {
+            int li = q[0]; // left index of range
+            int ri = q[1]; // right index of range
+            int ki = q[2]; // step size
+            int vi = q[3]; // multiplier value
+
+            // Update every ki-th index in range [li, ri]
+            for (int idx = li; idx <= ri; idx += ki) {
+                // Use 1LL to promote to long long before multiplication
+                nums[idx] = (1LL * nums[idx] * vi) % MOD;
+            }
+        }
+
+        // After applying all queries, calculate XOR of entire array
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+
+        return result;
+    }
+};
+
+
+
+// TC=O(n * query), SC=O(1)
