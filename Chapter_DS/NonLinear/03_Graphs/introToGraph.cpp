23f136562b48f6dc43383cd26b8d9d2f41fdf562
Ankit Kumar <ankit485803@gmail.com>
2025-08-27 18:37:39 +0530

    start newChapDS: Intro to Graph and its types

---
+++ Chapter_DS/NonLinear/03_Graphs/introToGraph.cpp
@@ -0,0 +1,20 @@
+
+
+
+/*
+
+27th Aug 2025 (Wednesday - Happy Ganesh Charthu Puja) start new DS - Graphs vvvv imp for placemnt/interview
+
+
+Graphs: networks of nodes, this is simple (how vertex/edges connection, which algo should apply, many edge cases)
+
+practical life eg: google map, uber, ola, Rapido
+
+
+
+Type of graphs: on the basis of edges - uni dirction, undirected graph (bidirectional)
+on the edgesWeight basis: weighted and unweighted graph; become 4 types 
+
+and two more type of graphs: connected and disconnected graphs
+
+*/
