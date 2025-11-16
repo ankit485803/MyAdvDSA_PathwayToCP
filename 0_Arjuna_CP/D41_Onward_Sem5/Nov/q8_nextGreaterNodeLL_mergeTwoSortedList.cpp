10885067c5d51fa9c1d897984bad97540505e59b
Ankit Kumar <ankit485803@gmail.com>
2025-11-16 16:37:10 +0530

    :

--- 0_Arjuna_CP/D41_Onward_Sem5/Nov/q8_nextGreaterNodeLL_mergeTwoSortedList.cpp
+++ 0_Arjuna_CP/D41_Onward_Sem5/Nov/q8_nextGreaterNodeLL_mergeTwoSortedList.cpp
@@ -354,3 +354,236 @@ public:
 
 
 
+/*
+
+qno 137  https://leetcode.com/problems/single-number-ii/
+
+
+137. Single Number II
+Medium
+Topics
+
+Companies
+Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,3,2]
+Output: 3
+Example 2:
+
+Input: nums = [0,1,0,1,0,1,99]
+Output: 99
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-231 <= nums[i] <= 231 - 1
+Each element in nums appears exactly three times except for one element which appears once.
+ 
+Discover more
+Software development tools
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+815,730/1.2M
+Acceptance Rate
+66.2%
+Topics
+Array
+Bit Manipulation
+
+
+*/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { //TC=O(n), SC=O(1)
+        int ones = 0, twos = 0;
+        
+        for (int num : nums) {
+            // `twos` will store the bits that appeared twice.
+            twos |= ones & num;
+            
+            // `ones` will store the bits that appeared once.
+            ones ^= num;
+
+            
+            // If a bit appears three times, clear it from both `ones` and `twos`
+            int threes = ones & twos;
+            ones &= ~threes;
+            twos &= ~threes;
+        }
+        
+        return ones;  
+    }
+};
+
+
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { //TC=O(n logN), SC=O(1)
+        sort(nums.begin(), nums.end());
+        
+
+        for (int i = 0; i < nums.size(); i++) {
+            // Check if we are at the start or middle of a triplet
+            if (i == 0 || nums[i] != nums[i-1]) {
+                // If the current number is not equal to the previous one, it is the unique one
+
+                if (i == nums.size() - 1 || nums[i] != nums[i+1]) {
+                    return nums[i];
+                }
+            }
+        }
+        
+
+        return -1; 
+
+    }
+};
+
+
+/*
+
+qno 3158   https://leetcode.com/problems/find-the-xor-of-numbers-which-appear-twice/
+
+3158. Find the XOR of Numbers Which Appear Twice
+Easy
+Topics
+
+Companies
+Hint
+You are given an array nums, where each number in the array appears either once or twice.
+
+Return the bitwise XOR of all the numbers that appear twice in the array, or 0 if no number appears twice.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,1,3]
+
+Output: 1
+
+Explanation:
+
+The only number that appears twice in nums is 1.
+
+Example 2:
+
+Input: nums = [1,2,3]
+
+Output: 0
+
+Explanation:
+
+No number appears twice in nums.
+
+Example 3:
+
+Input: nums = [1,2,2,1]
+
+Output: 3
+
+Explanation:
+
+Numbers 1 and 2 appeared twice. 1 XOR 2 == 3.
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 50
+1 <= nums[i] <= 50
+Each number in nums appears either once or twice.
+ 
+Discover more
+Programming language reference cards
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+69,704/89K
+Acceptance Rate
+78.3%
+Topics
+Array
+Hash Table
+Bit Manipulation
+Biweekly Contest 131
+
+
+*/
+
+class Solution {
+public:
+    int duplicateNumbersXOR(vector<int>& nums) { //TC=O(n logN), SC=O(1)
+        
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+
+        for(int i=0; i<nums.size()-1; i++) {
+            // Check if the current number is the same as the next one (duplicate)
+            if(nums[i] == nums[i+1]) {
+                ans ^= nums[i];  // XOR the duplicateNo
+                i++;  // Skip the nextElem since it's a duplicate
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+
+
+/*
+
+qno  260  https://leetcode.com/problems/single-number-iii/description/
+
+260. Single Number III
+Medium
+Topics
+
+Companies
+Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
+
+You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,1,3,2,5]
+Output: [3,5]
+Explanation:  [5, 3] is also a valid answer.
+Example 2:
+
+Input: nums = [-1,0]
+Output: [-1,0]
+Example 3:
+
+Input: nums = [0,1]
+Output: [1,0]
+ 
+
+Constraints:
+
+2 <= nums.length <= 3 * 104
+-231 <= nums[i] <= 231 - 1
+Each integer in nums will appear twice, only two integers will appear once.
+
+
+*/
