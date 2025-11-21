07e9269daa6d68735c0e5f02ea212cfd8208e202
Ankit Kumar <ankit485803@gmail.com>
2025-11-21 18:37:57 +0530

    keep multiplying found values by two prob

--- 0_Arjuna_CP/D41_Onward_Sem5/Nov/q13_reorderList_sortList_IsSeq_repeatedStrPattern.cpp
+++ 0_Arjuna_CP/D41_Onward_Sem5/Nov/q13_reorderList_sortList_IsSeq_repeatedStrPattern.cpp
@@ -374,6 +374,98 @@ Efficient Check:
 
     
 Note: In C++, string::npos is a special constant used to represent an invalid position or "not found" value when working with string operations, particularly in the find method.
-    
+
 */
 
+
+
+
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {  //TC=O(n) = SC due to the creation of the doubled string
+        int n = s.length();
+        // Create a new string by concatenating s with itself
+        string doubled = s + s;
+        
+        // Find the first occurrence of s in doubled, starting from index 1 and ending before the last character
+        // We are checking if `s` is a substring in the new string (excluding the first and last characters)
+        return doubled.substr(1, 2 * n - 2).find(s) != string::npos;
+    }
+};
+
+
+
+/*
+
+qno 2154  https://leetcode.com/problems/keep-multiplying-found-values-by-two/description/  
+
+
+2154. Keep Multiplying Found Values by Two
+Attempted
+Easy
+Topics
+
+Companies
+Hint
+You are given an array of integers nums. You are also given an integer original which is the first number that needs to be searched for in nums.
+
+You then do the following steps:
+
+If original is found in nums, multiply it by two (i.e., set original = 2 * original).
+Otherwise, stop the process.
+Repeat this process with the new number as long as you keep finding the number.
+Return the final value of original.
+
+ 
+
+Example 1:
+
+Input: nums = [5,3,6,1,12], original = 3
+Output: 24
+Explanation: 
+- 3 is found in nums. 3 is multiplied by 2 to obtain 6.
+- 6 is found in nums. 6 is multiplied by 2 to obtain 12.
+- 12 is found in nums. 12 is multiplied by 2 to obtain 24.
+- 24 is not found in nums. Thus, 24 is returned.
+Example 2:
+
+Input: nums = [2,7,9], original = 4
+Output: 4
+Explanation:
+- 4 is not found in nums. Thus, 4 is returned.
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+1 <= nums[i], original <= 1000
+
+
+*/
+
+
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) { //TC=O(n) SC=O(1)
+        
+        // Continue the process as long as 'original' is found in 'nums'
+        while(find(nums.begin(), nums.end(), original)  !=  nums.end()) {
+            original = 2*original;  // Multiply 'original' by 2 if it's found
+
+        }
+
+        return original;
+    }
+};
+
+
+
+/*
+
+qno 2309  https://leetcode.com/problems/greatest-english-letter-in-upper-and-lower-case/
+
+
+
+
+
+*/
