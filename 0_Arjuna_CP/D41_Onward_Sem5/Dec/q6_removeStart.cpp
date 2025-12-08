09ed2c4e34fb6ce36b52c97b15dada39c492ccc2
Ankit Kumar <ankit485803@gmail.com>
2025-12-08 22:16:48 +0530

    removeStart easyProb

---
+++ 0_Arjuna_CP/D41_Onward_Sem5/Dec/q6_removeStart.cpp
@@ -0,0 +1,104 @@
+
+
+
+/*
+
+8th Dec 2025 (Monday)
+
+
+qno  2390   https://leetcode.com/problems/removing-stars-from-a-string/
+
+
+
+
+2390. Removing Stars From a String
+Medium
+Topics
+
+Companies
+Hint
+You are given a string s, which contains stars *.
+
+In one operation, you can:
+
+Choose a star in s.
+Remove the closest non-star character to its left, as well as remove the star itself.
+Return the string after all stars have been removed.
+
+Note:
+
+The input will be generated such that the operation is always possible.
+It can be shown that the resulting string will always be unique.
+ 
+
+Example 1:
+
+Input: s = "leet**cod*e"
+Output: "lecoe"
+Explanation: Performing the removals from left to right:
+- The closest character to the 1st star is 't' in "leet**cod*e". s becomes "lee*cod*e".
+- The closest character to the 2nd star is 'e' in "lee*cod*e". s becomes "lecod*e".
+- The closest character to the 3rd star is 'd' in "lecod*e". s becomes "lecoe".
+There are no more stars, so we return "lecoe".
+Example 2:
+
+Input: s = "erase*****"
+Output: ""
+Explanation: The entire string is removed, so we return an empty string.
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters and stars *.
+The operation above can be performed on s.
+
+
+
+*/
+
+
+
+class Solution {
+public:
+    string removeStars(string s) {  //tc=O(N)=SC   submitted but give memory limit error
+        stack<char> st;  
+        for (char c : s) {
+            if (c == '*') {
+                st.pop();  // Pop lastChar before the star
+            } else {
+                st.push(c);  // Push currentChar to the stack
+            }
+        }
+
+        
+        // Now build the result string from stack
+        string result = "";
+        while (!st.empty()) {
+            result = st.top() + result;  // BuildStr in reverseOrder
+            st.pop();
+        }
+        
+        return result;
+    }
+};
+
+
+
+
+class Solution {
+public:
+    string removeStars(string s) {   //tc=O(n) = sc length of str
+        string result = ""; 
+        
+        for (char c : s) {
+            if (c == '*') {
+                result.pop_back();  // Remove lastChar (equivalent to pop).
+            } else {
+                result.push_back(c);  // Add non-star char to result.
+            }
+        }
+        
+        return result;  
+    }
+};
