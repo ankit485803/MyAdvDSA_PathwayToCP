a1ee70d506fdadc89b71f0696bc53edafdc1b8b4
Ankit Kumar <ankit485803@gmail.com>
2025-12-11 21:33:43 +0530

    countTripletes

--- 0_Arjuna_CP/D41_Onward_Sem5/Dec/q9_countOdd_countTriples.cpp
+++ 0_Arjuna_CP/D41_Onward_Sem5/Dec/q9_countOdd_countTriples.cpp
@@ -97,9 +97,55 @@ qno 1925  https://leetcode.com/problems/count-square-sum-triples/description/
 
 
 
+1925. Count Square Sum Triples
+Easy
+Topics
 
+Companies
+Hint
+A square triple (a,b,c) is a triple where a, b, and c are integers and a2 + b2 = c2.
+
+Given an integer n, return the number of square triples such that 1 <= a, b, c <= n.
+
+ 
+
+Example 1:
+
+Input: n = 5
+Output: 2
+Explanation: The square triples are (3,4,5) and (4,3,5).
+Example 2:
+
+Input: n = 10
+Output: 4
+Explanation: The square triples are (3,4,5), (4,3,5), (6,8,10), and (8,6,10).
+ 
+
+Constraints:
+
+1 <= n <= 250
 
 
 
 
 */
+
+
+class Solution {
+public:
+    int countTriples(int n) {  //tc=o(n*n), sc=O(1)
+        int count = 0;
+
+        for (int a = 1; a <= n; a++) {
+            for (int b = 1; b <= n; b++) {
+                int c2 = a*a + b*b;
+                int c = sqrt(c2);
+                if (c <= n && c*c == c2) {  //codition Req
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
