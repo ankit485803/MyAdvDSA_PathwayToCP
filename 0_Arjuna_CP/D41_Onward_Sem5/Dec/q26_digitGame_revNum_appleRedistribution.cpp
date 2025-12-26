8cba2173af940395e9b919ad3240260d457bd5b0
Ankit Kumar <ankit485803@gmail.com>
2025-12-26 16:52:37 +0530

    maxHappiness, addTwoNo probSolved

--- 0_Arjuna_CP/D41_Onward_Sem5/Dec/q26_digitGame_revNum_appleRedistribution.cpp
+++ 0_Arjuna_CP/D41_Onward_Sem5/Dec/q26_digitGame_revNum_appleRedistribution.cpp
@@ -365,3 +365,311 @@ public:
 
 
 
+
+
+/*
+
+
+
+
+qno 2  https://leetcode.com/problems/add-two-numbers/
+
+
+2. Add Two Numbers
+Solved
+Medium
+Topics
+
+Companies
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+ 
+
+Constraints:
+
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+ 
+
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+6,539,442/13.8M
+Acceptance Rate
+47.5%
+Topics
+Linked List
+Math
+Recursion
+
+
+
+*/
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);  // dummy node to simplify code
+        ListNode* current = dummy;          // pointer to construct the result list
+        int carry = 0;                      // to keep track of carry-over
+        
+
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int sum = carry;  // start with carry from previous operation
+            
+
+            // If l1 is not null, add its value to sum
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;  // move to the next node in l1
+            }
+            
+            // If l2 is not null, add its value to sum
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;  // move to the next node in l2
+            }
+            
+            // Update carry for the next addition (sum / 10 gives the carry)
+            carry = sum / 10;
+            
+            // Create a new node with the digit (sum % 10)
+            current->next = new ListNode(sum % 10);
+            current = current->next;  // move to the new node
+        }
+        
+        return dummy->next;  // the result starts from dummy->next
+    }
+};
+
+
+//TC=(max(n, m)) = sc
+
+
+
+
+/*
+
+
+
+
+qno 445  https://leetcode.com/problems/add-two-numbers-ii/description/
+
+
+445. Add Two Numbers II
+Medium
+Topics
+
+Companies
+You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+
+Input: l1 = [7,2,4,3], l2 = [5,6,4]
+Output: [7,8,0,7]
+Example 2:
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [8,0,7]
+Example 3:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+ 
+
+Follow up: Could you solve it without reversing the input lists?
+
+
+
+
+*/
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) { //tc=O(n+m) = sc 
+        stack<int> s1, s2;
+
+        // Push values of l1 into stack
+        while (l1) {
+            s1.push(l1->val);
+            l1 = l1->next;
+        }
+
+        // Push values of l2 into stack
+        while (l2) {
+            s2.push(l2->val);
+            l2 = l2->next;
+        }
+
+        int carry = 0;
+        ListNode* head = nullptr;
+
+        while (!s1.empty() || !s2.empty() || carry) {
+            int sum = carry;
+
+            if (!s1.empty()) {
+                sum += s1.top();
+                s1.pop();
+            }
+
+            if (!s2.empty()) {
+                sum += s2.top();
+                s2.pop();
+            }
+
+            carry = sum / 10;
+
+            // Insert at front
+            ListNode* node = new ListNode(sum % 10);
+            node->next = head;
+            head = node;
+        }
+
+        return head;
+    }
+};
+
+
+
+
+/*
+
+
+
+qno 3075   https://leetcode.com/problems/maximize-happiness-of-selected-children/description/
+
+
+3075. Maximize Happiness of Selected Children
+Solved
+Medium
+Topics
+
+Companies
+Hint
+You are given an array happiness of length n, and a positive integer k.
+
+There are n children standing in a queue, where the ith child has happiness value happiness[i]. You want to select k children from these n children in k turns.
+
+In each turn, when you select a child, the happiness value of all the children that have not been selected till now decreases by 1. Note that the happiness value cannot become negative and gets decremented only if it is positive.
+
+Return the maximum sum of the happiness values of the selected children you can achieve by selecting k children.
+
+ 
+
+Example 1:
+
+Input: happiness = [1,2,3], k = 2
+Output: 4
+Explanation: We can pick 2 children in the following way:
+- Pick the child with the happiness value == 3. The happiness value of the remaining children becomes [0,1].
+- Pick the child with the happiness value == 1. The happiness value of the remaining child becomes [0]. Note that the happiness value cannot become less than 0.
+The sum of the happiness values of the selected children is 3 + 1 = 4.
+Example 2:
+
+Input: happiness = [1,1,1,1], k = 2
+Output: 1
+Explanation: We can pick 2 children in the following way:
+- Pick any child with the happiness value == 1. The happiness value of the remaining children becomes [0,0,0].
+- Pick the child with the happiness value == 0. The happiness value of the remaining child becomes [0,0].
+The sum of the happiness values of the selected children is 1 + 0 = 1.
+Example 3:
+
+Input: happiness = [2,3,4,5], k = 1
+Output: 5
+Explanation: We can pick 1 child in the following way:
+- Pick the child with the happiness value == 5. The happiness value of the remaining children becomes [1,2,3].
+The sum of the happiness values of the selected children is 5.
+ 
+
+Constraints:
+
+1 <= n == happiness.length <= 2 * 105
+1 <= happiness[i] <= 108
+1 <= k <= n
+
+
+
+*/
+
+
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {  //tc=O(nLogN), sc=O(1)
+        int n = happiness.size();
+        sort(happiness.begin(), happiness.end());
+
+        long long maxHapp = 0;
+        int dec = 0;
+        for(int i=n-1; i>=n-k; i--) {
+            int reduced = happiness[i] - dec;
+            dec++;
+
+            if(reduced > 0) {
+                maxHapp += reduced;
+            } else {
+                break;
+            }
+        }
+
+        return maxHapp;
+    }
+};
