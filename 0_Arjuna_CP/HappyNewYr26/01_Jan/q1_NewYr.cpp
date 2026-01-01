8000989b22f5e94163c6473490221408f27a6710
Ankit Kumar <ankit485803@gmail.com>
2026-01-01 08:02:20 +0530

    HappyNewYear2026 and make the separate repo directory for CP

---
+++ 0_Arjuna_CP/HappyNewYr26/01_Jan/q1_NewYr.cpp
@@ -0,0 +1,16 @@
+
+
+
+
+
+/*
+
+1st Jan 2026 (Thursday - HappyNewYear 2026)
+
+
+
+
+
+
+
+*/
---
+++ 0_Arjuna_CP/HappyNewYr26/newYr2026_Thursday.cpp
@@ -0,0 +1,22 @@
+
+
+
+
+
+/*
+
+
+1st Jan 2026 (Thursday - HappyNewYear 2026)
+
+after endSem5paper 3rd year IITP and target as developer/scientist MNC FAANG
+
+
+
+
+
+
+
+
+*/
+
+git reset --soft HEAD~1
