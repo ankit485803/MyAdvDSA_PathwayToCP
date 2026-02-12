0d112cf952bca437b4ed0f64ecc74b3e0ab8d239
Ankit Kumar <ankit485803@gmail.com>
2026-02-12 16:23:16 +0530

    convert the idexing part from a, b to ch1, ch2 format

--- 02_SigmaApnaCollege/ch1_CourseIntro/msg_PlacementIntern.cpp
+++ 02_SigmaApnaCollege/ch1_CourseIntro/msg_PlacementIntern.cpp
@@ -2,6 +2,8 @@
 
 g++ demo.cpp -o myRunFile
 .\myRunFile.exe
+rm *.exe
+Remove-Item .\demo.exe, .\myRun.exe, .\myRunFile.exe, .\myRunFile2.exe
 
 
 /*
---
+++ 0_Arjuna_CP/HappyNewYr26/02_Feb/q12_sumOfLeftLeaves.cpp
@@ -0,0 +1,16 @@
+
+
+
+/*
+
+
+12th Feb 2026 (Thursday)
+
+
+qno 404   https://leetcode.com/problems/sum-of-left-leaves/
+
+
+
+
+
+*/
