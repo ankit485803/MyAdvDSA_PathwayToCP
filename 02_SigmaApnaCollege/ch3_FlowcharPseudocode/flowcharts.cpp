e17faee50db84e56292269931dd9d22ec92b3ffe
Ankit Kumar <ankit485803@gmail.com>
2026-02-15 19:34:41 +0530

    coveredHalf of ch3_FlowchartPseudocode

---
+++ 02_SigmaApnaCollege/ch3_FlowcharPseudocode/flowcharts.cpp
@@ -0,0 +1,71 @@
+sanja@IITP:~$ cd /mnt/c
+sanja@IITP:/mnt/c$ cd Users/sanja/Desktop/Lab/
+sudo apt update
+sudo apt install g++
+
+
+
+
+PS C:\Users\sanja\Desktop\Lab> notepad .\sigmaApnaCollege.cpp
+PS C:\Users\sanja\Desktop\Lab> g++ .\sigmaApnaCollege.cpp -o .\myRunFile.exe
+PS C:\Users\sanja\Desktop\Lab> .\myRunFile.exe
+Enter a: 3
+Enter b: 8
+Sum = 11
+PS C:\Users\sanja\Desktop\Lab>
+
+
+/*
+
+
+15th Feb 2026 (Sunday mahaShivRatri- fistChapter)
+
+In this hm koi code nahi lekhege waske behind the how to form logic hota hai
+
+Flowchart: are diagrams to represent solution of a problem.
+small part, logicallyArrange, 
+
+
+
+#COMPONENT of flowchart: start/exit, inut/output, process, decision, arrows
+e.g. sum of twoSum
+
+
+PseudoCode: nakli code
+
+
+
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+
+    int sumOfTwoNum = a + b;
+
+    cout << "Sum = " << sumOfTwoNum << endl;
+
+    return 0;
+}
+
+
+
+
+
+sanja@IITP:/mnt/c/Users/sanja/Desktop/Lab$ ls
+mapper.py  myRunFile.exe  reducer.py  scrape.py  sigmaApnaCollege.cpp  sigmaApnaCollege.cppc  wiki.txt
+sanja@IITP:/mnt/c/Users/sanja/Desktop/Lab$ g++ sigmaApnaCollege.cpp -o runFile
+sanja@IITP:/mnt/c/Users/sanja/Desktop/Lab$ ./runFile
+Enter a: 5
+Enter b: 3
+Sum = 8
+sanja@IITP:/mnt/c/Users/sanja/Desktop/Lab$
---
+++ 02_SigmaApnaCollege/ch3_FlowcharPseudocode/sumOfTwoNumb.cpp
@@ -0,0 +1,22 @@
+
+// nano fileName.cpp
+// ctrl + o and ctrl + x
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+
+    int sumOfTwoNum = a + b;
+
+    cout << "Sum = " << sumOfTwoNum << endl;
+
+    return 0;
+}
