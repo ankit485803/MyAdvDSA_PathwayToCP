3e5aed180c049bbe2b373761a39b005d622ce550
Ankit Kumar <ankit485803@gmail.com>
2026-03-08 13:41:00 +0530

    MCQ through googleForm by codeBash event by KushalVijay

---
+++ Days/D17_KushalVijay_codebaseIITP_HackerRank/mcq/total 15 ques.cpp
@@ -0,0 +1,18 @@
+
+
+/*
+
+8th March 2026 (Sunday)
+
+
+goolge form link: https://docs.google.com/forms/u/0/d/e/1FAIpQLSdyctFGJsF3222PVhpAjdhHkAaxYSJ3UYNsbO9dNjP40OuhKA/formResponse
+
+
+CodeBash - IIT Patna MCQ Quiz
+MCQ Quiz for CodeBash event at IIT Patna.
+Answer all 15 questions. Each question carries 1 point.
+Total: 15 points
+
+
+
+*/
