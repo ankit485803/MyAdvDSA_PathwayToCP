ce3b50cca8847447d558689423dffff797ad0ff0
Ankit Kumar <ankit485803@gmail.com>
2026-04-02 18:09:41 +0530

    recap all the concepts taught till now sigmaApnaCollege and due to midSemReportSubmission of capstoneProj not able to continute for 5 days

---
+++ 02_SigmaApnaCollege/ch8_Patterns/star/fromScratchStar.cpp
@@ -0,0 +1,35 @@
+
+
+/*
+
+2nd April 2026 (Thursday - midSemCapstoneProj reportSubmission)
+
+print the InvertedStar pattern using generalize formual
+
+
+shardhaDi ne boli es tarah ke prob mail innerLoop mai formula kuch es tarh hoga: j = n-i, n-i+1, n-i-1 
+
+
+****
+***
+**
+*
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    for(int i=1; i<=n; i++) {  //outerLoop-no of rows
+        for(int j=1; j <= n-i+1; j++) {  //innerLoop- kya print karna hai aur kab tak karna hai
+            cout << "*" << " "; 
+        }
+        cout << endl;  //jab innerLoop end ho baha jane se pahle newline hona chahiye 
+    }
+
+    return 0;
+}
