059eb4b3892df8caa9ed22d5bb13fafc7ace0a61
Ankit Kumar <ankit485803@gmail.com>
2026-04-05 10:43:39 +0530

    judgeCircleRobotReturnToOrigin prob

--- 0_Arjuna_CP/HappyNewYr26/04_March/q5_judgeCircleRobotReturnToOrigin.cpp
+++ 0_Arjuna_CP/HappyNewYr26/04_March/q5_judgeCircleRobotReturnToOrigin.cpp
@@ -56,3 +56,27 @@ Simulation
 
 
 */
+
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {  //TC=O(n), SC=O(1)
+        //startWithOrigin
+        int x=0, y=0;
+
+        for(char mov : moves) {
+            if(mov == 'U') {
+                y++;
+            } else if(mov == 'D') {
+                y--; //move down decrease y coord
+
+            } else if(mov == 'L') {
+                x--;  
+            } else if(mov == 'R') {
+                x++;
+            }
+        }
+
+        return (x == 0 && y == 0);
+    }
+};
