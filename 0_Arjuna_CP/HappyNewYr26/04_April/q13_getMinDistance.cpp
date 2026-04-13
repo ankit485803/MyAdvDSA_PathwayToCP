50f6a61d6858eade6b59177f372f540d136ed6a9
Ankit Kumar <ankit485803@gmail.com>
2026-04-13 17:07:33 +0530

    getMinDistance prob solved and optimized the solution

--- 0_Arjuna_CP/HappyNewYr26/04_April/q13_getMinDistance.cpp
+++ 0_Arjuna_CP/HappyNewYr26/04_April/q13_getMinDistance.cpp
@@ -111,3 +111,42 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int ans = INT_MAX;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == target) {
+                int dist = abs(i - start);
+                
+                if(dist == 0) return 0;  // 🔥 early exit
+                
+                ans = min(ans, dist);
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        
+        if(nums[start] == target) return 0;  // 🚀 fastest case
+
+        int ans = INT_MAX;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == target) {
+                ans = min(ans, abs(i - start));
+            }
+        }
+
+        return ans;
+    }
+};
