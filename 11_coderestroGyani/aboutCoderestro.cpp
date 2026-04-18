40252114af35ef3d661c4318e950ad42f5764b99
Ankit Kumar <ankit485803@gmail.com>
2026-04-18 15:59:15 +0530

    setup for coderestroGyani sepecificDirectory concepts

---
+++ 11_coderestroGyani/aboutCoderestro.cpp
@@ -0,0 +1,25 @@
+
+
+/*
+
+18th April 2026 (Saturday)
+
+About Coderestro - https://www.instagram.com/coderestro/
+
+who shared the CP concepts daily 
+
+By Gyani Kumari (CSE NITP'21 Google SDE 1)
+in.linkedin.com/in/gyani-kumari-102002
+https://www.youtube.com/@explorewithgyani578
+
+
+C:\Users\sanja\Desktop\GitProj\MyAdvDSA_PathwayToCP\11_coderestroGyani
+here c means concepts
+directoryNameList:
+
+c1_firstLastOccurrence
+c2_
+
+
+
+*/
---
+++ 11_coderestroGyani/c1_firstLastOccurrence/traverseEachElem_approchBruteForce.cpp
@@ -0,0 +1,13 @@
+
+
+/* Link: https://www.instagram.com/reels/DXMYZakgW8q/
+
+Problem Statement: 
+
+given  Arr[7] = {1, 2, 2, 2, 3, 3, 4}, target = 2
+find the fist and last occurrence index of target 
+
+
+method1: bruteForce - traverse eachElem  tc=O(n), sc=O(1)
+
+*/
