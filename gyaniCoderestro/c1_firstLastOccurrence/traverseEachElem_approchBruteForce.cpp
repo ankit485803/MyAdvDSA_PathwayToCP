2a2876dc6677dca38eef66be01182d47384996c9
Ankit Kumar <ankit485803@gmail.com>
2026-04-18 16:29:02 +0530

    done method1 traverseEachElem

--- gyaniCoderestro/c1_firstLastOccurrence/traverseEachElem_approchBruteForce.cpp
+++ gyaniCoderestro/c1_firstLastOccurrence/traverseEachElem_approchBruteForce.cpp
@@ -11,3 +11,37 @@ find the fist and last occurrence index of target
 method1: bruteForce - traverse eachElem  tc=O(n), sc=O(1)
 
 */
+
+#include <iostream>
+using namespace std;
+#include <vector>
+
+void firstLastOccurrence(vector<int>& arr, int target) {
+    int first = -1, last = -1;
+
+    for(int i=0; i < arr.size(); i++) {
+        if(arr[i] == target) {
+            if(first == -1) {
+                first = i;  //first occurrence 
+            }
+            last = i;  //keep updating last
+        }
+    }
+
+    cout << "First Index: " << first << endl;
+    cout << "Last Index of target:" << last << endl;
+}
+
+
+int main() {
+    //vector<int> arr = {1, 2, 2, 2, 3, 3, 4};
+    vector<int> arr(7);
+    arr = {1, 2, 2, 2, 3, 3, 4};
+    
+    int target = 2;
+
+    // callFunct
+    firstLastOccurrence(arr, target);
+
+    return 0;
+}
