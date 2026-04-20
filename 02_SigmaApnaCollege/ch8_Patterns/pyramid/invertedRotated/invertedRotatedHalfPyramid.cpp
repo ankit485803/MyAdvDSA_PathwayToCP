564524fb850d080a5e62ca49aaf34d3e5207323d
Ankit Kumar <ankit485803@gmail.com>
2026-04-20 18:23:45 +0530

    feat(sigmaApnaCollege): try to solve inverted Rotated half (ch8)

--- 02_SigmaApnaCollege/ch8_Patterns/pyramid/invertedRotated/invertedRotatedHalfPyramid.cpp
+++ 02_SigmaApnaCollege/ch8_Patterns/pyramid/invertedRotated/invertedRotatedHalfPyramid.cpp
@@ -16,3 +16,51 @@ draw the pattern Inverted & Rotated Half Pyramid
 
 
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    for(int i=1; i <= n; i++) {  //outerLoop no of rows
+        //yaha two innerLoop lagega 
+
+        //spaces
+        for(int s = i; s <= n - 1; s++) {
+            cout << " ";
+        }
+        cout << endl;
+
+        //second innerLoop for star
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
+
+
+/*
+currenOutput of thisCode
+
+sanja@IITP:~$ cd /mnt/c/Users/sanja/Desktop/LabSession/
+sanja@IITP:/mnt/c/Users/sanja/Desktop/LabSession$ notepad sigmaApnaCollege.cpp
+^Csanja@IITP:/mnt/c/Users/sanja/Desktop/LabSessiong++ sigmaApnaCollege.cpp -o codeRunFile.exe
+sanja@IITP:/mnt/c/Users/sanja/Desktop/LabSession$ ./codeRunFile.exe
+
+*
+
+**
+
+***
+
+****
+sanja@IITP:/mnt/c/Users/sanja/Desktop/LabSession$
+
+
+
+*/ 
