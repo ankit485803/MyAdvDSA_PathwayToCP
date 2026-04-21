03eadfd31468af49b35fe0e23a77706b6dd21bed
Ankit Kumar <ankit485803@gmail.com>
2026-04-21 17:41:02 +0530

    feat(sigmaApnaCollege): start solving Floyd's Triangle pattern and updates imp points in invertedRotatedHalfPyramid (ch8)

---
+++ 02_SigmaApnaCollege/ch8_Patterns/FloydsTriangle/patternFloydsTriangle.cpp
@@ -0,0 +1,19 @@
+
+
+/*
+
+21st April 2026 (Tuesday)
+
+print the pattern Floyd's Triangle
+
+
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14
+
+git commit -m "feat(sigmaApnaCollege): start solving Floyd's Triangle pattern and updates imp points in invertedRotatedHalfPyramid (ch8)"
+
+
+*/
--- 02_SigmaApnaCollege/ch8_Patterns/pyramid/invertedRotated/invertedRotatedHalfPyramid.cpp
+++ 02_SigmaApnaCollege/ch8_Patterns/pyramid/invertedRotated/invertedRotatedHalfPyramid.cpp
@@ -14,6 +14,8 @@ draw the pattern Inverted & Rotated Half Pyramid
 
 
 
+shardhaDi bolie thi ki kabhi kabhi two or three outerloop ke under two innterloop bhi ho skate hai but apko bigeger prob ko break karte
+small mai solve karo easy hoga for patter
 
 */
 
@@ -62,5 +64,45 @@ sanja@IITP:/mnt/c/Users/sanja/Desktop/LabSession$ ./codeRunFile.exe
 sanja@IITP:/mnt/c/Users/sanja/Desktop/LabSession$
 
 
-
+You should print both spaces and stars on the same line, and only then move to the next line.
+Each row = spaces + stars → then newline
 */ 
+
+
+
+int main() {
+    int n = 4;
+
+    for(int i=1; i <= n; i++) {  //outerLoop no of rows
+        //spaces
+        for(int s = i; s <= n - 1; s++) {
+            cout << " ";
+        }
+        //cout << endl;  //yaha hmko comments out karna hoga newline because ek to complete nahi hwua ha na es liye Ankit
+
+        //second innerLoop for star
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        // move to next line AFTER printing both
+        cout << endl;
+    }
+
+
+    return 0;
+}
+
+
+
+/*
+now output
+^Csanja@IITP:/mnt/c/Users/sanja/Desktop/LabSession$ g++ sigmaApnaCollege.cpp -o codeRunFile.exe
+sanja@IITP:/mnt/c/Users/sanja/Desktop/LabSession$ ./codeRunFile.exe
+   *
+  **
+ ***
+****
+sanja@IITP:/mnt/c/Users/sanja/Desktop/LabSession$
+
+
+*/
