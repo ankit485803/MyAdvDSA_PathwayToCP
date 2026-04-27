a92db63314dac337f431f5c19c244eea76597a99
Ankit Kumar <ankit485803@gmail.com>
2026-04-27 18:06:51 +0530

    feat(sigmaApnaCollege): added the assigQues and rename the someFiles

---
+++ 02_SigmaApnaCollege/ch8_Patterns/assig/q1_zerosOnesTriangle.cpp
@@ -0,0 +1,17 @@
+
+
+/*
+
+28th April 2026 (Tuesday)
+
+Question 1 : Print the 0-1 Triangle Pattern.
+For n = 5
+1
+01
+101
+0101
+10101
+
+
+
+*/
--- 02_SigmaApnaCollege/ch8_Patterns/introCh8_Patterns.cpp
+++ 02_SigmaApnaCollege/ch8_Patterns/introCh8_Patterns.cpp
@@ -7,8 +7,9 @@
 
 25th March 2026 (Wednesday)
 
+yah chapter interview ke ques mai nahi aayega but logic building mai bahut help karenag ok told by instructor shardhaDi
 
-
+in this chapter we will applied major nestedLoop concepts more
 
 
 */
