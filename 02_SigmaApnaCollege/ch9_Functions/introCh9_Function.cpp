f741512f4941f7eb1a3dfdb19399de5a60880d69
Ankit Kumar <ankit485803@gmail.com>
2026-05-05 19:03:52 +0530

    feat(sigmaApnaCollege): started newChapter Function from today (ch9)

--- 02_SigmaApnaCollege/ch9_Functions/introCh9_Function.cpp
+++ 02_SigmaApnaCollege/ch9_Functions/introCh9_Function.cpp
@@ -1,1 +1,25 @@
+git commit -m "feat(sigmaApnaCollege): started newChapter Function from today (ch9)"
 
+/*
+
+5th May 2026 (Tuesday) Intro to function
+
+WHY funct ?
+jab hm bachapan mai Ramamyan serial dekhte the waha pe directory bigMovies ko chunk of episodes mai break karke dekhate the
+similary when our codebase is going to increase to understanding ke liye we have to break
+
+
+definition: Block of code which runs when it is called
+
+
+
+
+void primitive data type in c++
+oid is a primitive (built-in) data type that represents a valueless entity or the absence of a value. Because it denotes nothingness, you cannot declare a variable of type void
+
+
+*/
+
+void printMessage() {
+    std::cout << "Hello, World!";
+}
