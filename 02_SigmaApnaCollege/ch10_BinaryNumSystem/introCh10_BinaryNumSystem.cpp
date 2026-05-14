5ff3af29e7f590709beb82d166514723abcd2f41
Ankit Kumar <ankit485803@gmail.com>
2026-05-14 18:38:08 +0530

    feat(sigmaApnaCollege): started newChap BinaryNumSystem from today (ch10)

---
+++ 02_SigmaApnaCollege/ch10_BinaryNumSystem/introCh10_BinaryNumSystem.cpp
@@ -0,0 +1,17 @@
+
+
+/*
+
+14th May 2026 (Thursday - sem6 goingToEnd)
+
+Binary means only two digits -  0, 1
+
+1 bit mai 0, 1
+8 bits = 1 byte 
+char = 1 byte
+
+*/
+
+
+
+git commit -m "feat(sigmaApnaCollege): started newChap BinaryNumSystem from today (ch10)"
