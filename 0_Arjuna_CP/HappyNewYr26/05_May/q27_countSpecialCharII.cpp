a51d9615328364cfdb905791a76fdd87923bd33b
Ankit Kumar <ankit485803@gmail.com>
2026-05-27 18:19:31 +0530

    countSpecialChar II leetcode probNo 3121 solved

---
+++ 0_Arjuna_CP/HappyNewYr26/05_May/q27_countSpecialCharII.cpp
@@ -0,0 +1,54 @@
+
+
+/*
+
+
+27th May 2026 (Wednesday)
+
+
+qno 3121  https://leetcode.com/problems/count-the-number-of-special-characters-ii/description/?envType=daily-question&envId=2026-05-27
+
+
+
+
+*/
+
+
+
+//Approach (Track last occurrence of lowercase and first occurrence of uppercase)
+//T.C : O(n)
+//S.C : O(1) - fixed-size arrays of length 26
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        
+        vector<int> lastSmall(26, -1);
+        vector<int> firstCapital(26, -1);
+
+        for(int i = 0; i < word.length(); i++) {
+            
+            char ch = word[i];
+
+            if(islower(ch)) {
+                lastSmall[ch - 'a'] = i;
+            }
+            else {
+                if(firstCapital[ch - 'A'] == -1) {
+                    firstCapital[ch - 'A'] = i;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for(int i = 0; i < 26; i++) {
+
+            if(lastSmall[i] != -1 && firstCapital[i] != -1 &&
+               lastSmall[i] < firstCapital[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
