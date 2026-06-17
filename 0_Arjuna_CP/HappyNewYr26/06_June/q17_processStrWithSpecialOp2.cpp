62979dd1b8e1c318bc52f9e176f4d7754fea998d
Ankit Kumar <ankit485803@gmail.com>
2026-06-17 15:05:21 +0530

    processStrWithSpecailOp II  LeetCode probNo 3614 and solved using the reverse simulation

---
+++ 0_Arjuna_CP/HappyNewYr26/06_June/q17_processStrWithSpecialOp2.cpp
@@ -0,0 +1,63 @@
+
+
+/*
+
+17th June 2026 (Wednesday)
+
+qno 3614  https://leetcode.com/problems/process-string-with-special-operations-ii/?envType=daily-question&envId=2026-06-17
+
+
+
+*/
+
+
+//Approach (Reverse Simulation)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    char processStr(string s, long long k) {
+        int n = s.length();
+
+        long long L = 0;
+
+        for(char &ch : s) {
+            if(ch == '*') {
+                if(L > 0)
+                    L--;
+            } else if(ch == '#') {
+                L *= 2;
+            } else if(ch == '%') {
+                //reverse - no change in L
+                continue;
+            } else { //'a' to 'z'
+                L++;
+            }
+        }
+
+        if(k >= L) {
+            return '.';
+        }
+
+        for(int i = n-1; i >= 0; i--) {
+            if(s[i] == '*') {
+                L++; //no change in k
+            } else if(s[i] == '%') {
+                //no change in L
+                k = L - k - 1;
+            } else if(s[i] == '#') {
+                L = L/2;
+                k = (k >= L) ? k - L : k;
+            } else { //'a' to 'z'
+                L--;
+            }
+
+            if(k == L) {
+                return s[i];
+            }
+        }
+
+        return '.';
+
+    }
+};
