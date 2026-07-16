c76f2c6791b200d483d5e352607d3ff803a66f47
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-07-16 17:37:34 +0530

    Create q16.cpp and Leetcode 3867. Sum of GCD of Formed Pairs

---
+++ 0_Arjuna_CP/HappyNewYr26/07_July/q16.cpp
@@ -0,0 +1,36 @@
+
+Leetcode 3867. Sum of GCD of Formed Pairs), 16th July, 2026
+
+
+
+
+//Approach - simultation
+//T.C : O(nlog(n)+nlog(M)), M = max element 
+//S.C : O(n)
+class Solution {
+public:
+    long long gcdSum(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> prefixGcd;
+        int maxEl = -1;
+        for(int i = 0; i < n; i++) {
+            maxEl = max(maxEl, nums[i]);
+            prefixGcd.push_back(__gcd(nums[i], maxEl));
+        }
+
+        sort(begin(prefixGcd), end(prefixGcd));
+
+        long long result = 0;
+        int i = 0;
+        int j = n-1;
+
+        while(i < j) {
+            result += __gcd(prefixGcd[i], prefixGcd[j]);
+            i++;
+            j--;
+        }
+
+        return result;
+    }
+};
