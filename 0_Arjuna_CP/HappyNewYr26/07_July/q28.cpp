7f51311c87951a62ff157ca4574a4ce1803eae82
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-07-28 20:15:21 +0530

    Create q28.cpp and smallest-palindromic-rearrangement-i/submissions/2084901466

---
+++ 0_Arjuna_CP/HappyNewYr26/07_July/q28.cpp
@@ -0,0 +1,21 @@
+qno 3517 https://leetcode.com/problems/smallest-palindromic-rearrangement-i/submissions/2084901466
+
+
+//Approach (Split in hald , Sort and form palindrome)
+//T.C : O(n log n)
+//S.C : O(1) (ignoring the space taken for sorting internally)
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n   = s.length();
+        int mid = n/2;
+
+        sort(s.begin(), s.begin() + mid); //note : mid is not included i.e. [begin, mid)
+
+        for (int i = 0; i < mid; i++) {
+            s[n - 1 - i] = s[i];
+        }
+
+        return s;
+    }
+};
