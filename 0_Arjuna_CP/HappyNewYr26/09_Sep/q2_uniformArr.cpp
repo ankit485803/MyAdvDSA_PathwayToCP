678f0e8da6c345287c70b84ff5a5096748f18939
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-09-02 18:42:12 +0530

    Create q2_uniformArr.cpp

---
+++ 0_Arjuna_CP/HappyNewYr26/09_Sep/q2_uniformArr.cpp
@@ -0,0 +1,12 @@
+
+// 2nd Sep 2026 (Wednesday)
+
+qno 3875 https://leetcode.com/problems/construct-uniform-parity-array-i
+
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
